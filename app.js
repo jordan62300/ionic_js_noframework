a9b524f2127c1182d76aaae0862e4ab6ab398e80
jordan62300 <jordanfievetpro@outlook.fr>
2019-11-14 04:24:37 +0100

    ajout de l'alert en cas de mauvaise valeur dans le formulaire

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const soireeList = document.getElementById("soiree-list")
 const buttonCancel = document.getElementById("cancel");
 const buttonAdd = document.getElementById("add");
 
+const alertCtrl = document.querySelector("ion-alert-controller");
+
 
 
 
@@ -25,6 +27,13 @@ function addSoiree() {
 
         if(nomdeSoiree.trim().length <= 0 || dateDeSoiree == null || themeDeSoiree == "Pas de theme"){
             console.log("ca marche pas")
+            alertCtrl.create({
+                message: 'Entrez des informations correctes',
+                header: 'Invalids Inputs',
+                buttons: ['Okay']
+            }).then(alertElement => {
+                alertElement.present()
+            })
         } else {
             const newItem = document.createElement('ion-item');
             newItem.innerHTML = "Nom de la soirée : " + nomdeSoiree;
